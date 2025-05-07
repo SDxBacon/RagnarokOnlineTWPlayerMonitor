1dee6d8d00e95012d911c5fa3ae834c837bd9e8a
SDxBacon <z141134553@gmail.com>
2025-05-07 18:42:17 +0800

    refactor: 💡 shrink the ui window size

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:    "myproject",
-		Width:    1024,
-		Height:   768,
+		Width:    720,
+		Height:   360,
 		LogLevel: logger.INFO,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
