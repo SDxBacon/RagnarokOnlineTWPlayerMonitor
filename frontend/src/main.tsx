b7e579dbe29500fabf2a8191dea6724749984d3c
SDxBacon <z141134553@gmail.com>
2025-05-08 15:19:27 +0800

    feat: 🎸 add ThemeProvider and set dark mode as default

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import './style.css'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "./components/ThemeProvider";
+import App from "./App";
+import "./style.css";
 
-const container = document.getElementById('root')
+const container = document.getElementById("root");
 
-const root = createRoot(container!)
+const root = createRoot(container!);
 
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
