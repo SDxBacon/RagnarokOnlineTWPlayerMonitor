3eebfc8cf93cffa9436b91124c2e45af656911b4
SDxBacon <z141134553@gmail.com>
2025-05-22 23:11:52 +0800

    feat: 🎸 disable resize app

--- main.go
+++ main.go
@@ -49,6 +49,7 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
+		DisableResize:    true,
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
